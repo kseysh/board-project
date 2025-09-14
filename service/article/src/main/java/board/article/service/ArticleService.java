f346b12f43012e445755bd23a1d6a0408a4d2d26
kseysh <kseysh123@naver.com>
2025-09-14 17:20:47 +0900

    feat: 게시글 개수 조회 API 구현

--- service/article/src/main/java/board/article/service/ArticleService.java
+++ service/article/src/main/java/board/article/service/ArticleService.java
@@ -3,7 +3,9 @@ package board.article.service;
 import static board.article.entity.ArticleConstant.MOVABLE_PAGE_COUNT;
 
 import board.article.entity.Article;
+import board.article.entity.BoardArticleCount;
 import board.article.repository.ArticleRepository;
+import board.article.repository.BoardArticleCountRepository;
 import board.article.service.request.ArticleCreateRequest;
 import board.article.service.request.ArticleUpdateRequest;
 import board.article.service.response.ArticlePageResponse;
@@ -22,9 +24,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class ArticleService {
     private final Snowflake snowflake = Snowflake.getInstance();
     private final ArticleRepository articleRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
 
+    @Transactional
     public ArticleResponse create(ArticleCreateRequest request) {
         Article article = articleRepository.save(Article.create(snowflake.nextId(), request));
+        int result = boardArticleCountRepository.increase(request.getBoardId());
+        if(result == 0){
+            boardArticleCountRepository.save(
+                    BoardArticleCount.init(request.getBoardId(), 1L)
+            );
+        }
         return ArticleResponse.from(article);
     }
 
@@ -32,20 +42,22 @@ public class ArticleService {
     public ArticleResponse update(Long articleId, ArticleUpdateRequest request) {
         Article article = articleRepository.findById(articleId).orElseThrow();
         article.update(request.getTitle(), request.getContent());
-        articleRepository.save(article);
         return ArticleResponse.from(article);
     }
 
+    @Transactional(readOnly = true)
     public ArticleResponse read(Long articleId) {
-        Article article = articleRepository.findById(articleId).orElseThrow();
-        article.update(article.getTitle(), article.getContent());
-        return ArticleResponse.from(article);
+        return ArticleResponse.from(articleRepository.findById(articleId).orElseThrow());
     }
 
+    @Transactional
     public void delete(Long articleId) {
+        Article article = articleRepository.findById(articleId).orElseThrow();
         articleRepository.deleteById(articleId);
+        boardArticleCountRepository.decrease(article.getBoardId());
     }
 
+    @Transactional(readOnly = true)
     public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
         Long offset = (page - 1) * pageSize;
         return ArticlePageResponse.of(
@@ -59,6 +71,7 @@ public class ArticleService {
         );
     }
 
+    @Transactional(readOnly = true)
     public List<ArticleResponse> readAllWithInfiniteScroll(Long boardId, Long pageSize, Optional<Long> lastArticleId) {
         List<Article> articles = lastArticleId.isPresent() ?
                 articleRepository.findAllWithInfiniteScroll(boardId, pageSize, lastArticleId.get()) :
@@ -66,7 +79,15 @@ public class ArticleService {
         return articles.stream().map(ArticleResponse::from).toList();
     }
 
+    @Transactional(readOnly = true)
     public PreviousArticleIdResponse findNextArticleIdAfter(Long boardId, Long articleId) {
         return PreviousArticleIdResponse.of(articleRepository.findNextIdAfter(boardId, articleId));
+    }
+
+    @Transactional(readOnly = true)
+    public Long count(Long boardId) {
+        return boardArticleCountRepository.findById(boardId)
+                .map(BoardArticleCount::getArticleCount)
+                .orElse(0L);
     }
 }
