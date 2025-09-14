ae58d0084b8f7a1394fa849543329cec81f236e2
kseysh <kseysh123@naver.com>
2025-09-15 00:34:48 +0900

    feat: 조회수 증가시 동시성 문제 Test

--- service/view/src/test/java/board/view/controller/ViewApiTest.java
+++ service/view/src/test/java/board/view/controller/ViewApiTest.java
@@ -1,5 +1,7 @@
 package board.view.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestClient;
@@ -17,10 +19,15 @@ class ViewApiTest {
         ExecutorService executorService = Executors.newFixedThreadPool(100);
         CountDownLatch latch = new CountDownLatch(10000);
 
+        Long articleId = 5L;
+
+        Long beforeCount = count(articleId);
+        log.info("count = {}", beforeCount);
+
         for(int i=0; i<10000; i++) {
             executorService.submit(() -> {
                 restClient.post()
-                        .uri("/v1/article-views/articles/{articleId}/users/{userId}", 4L, 1L)
+                        .uri("/v1/article-views/articles/{articleId}/users/{userId}", articleId, 1L)
                         .retrieve()
                         .toBodilessEntity();
                 latch.countDown();
@@ -29,11 +36,16 @@ class ViewApiTest {
 
         latch.await();
 
-        Long count = restClient.get()
-                .uri("/v1/article-views/articles/{articleId}/count", 4L)
+        Long afterCount = count(articleId);
+        log.info("count = {}", afterCount);
+
+        assertEquals(afterCount, beforeCount + 1);
+    }
+
+    Long count(Long articleId) {
+        return restClient.get()
+                .uri("/v1/article-views/articles/{articleId}/count", articleId)
                 .retrieve()
                 .body(Long.class);
-
-        log.info("count = {}", count);
     }
 }
