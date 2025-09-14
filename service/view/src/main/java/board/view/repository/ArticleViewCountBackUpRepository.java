4c61a40c43082d121a4a6e9c898d963b1ab6ee5a
kseysh <kseysh123@naver.com>
2025-09-14 18:01:59 +0900

    feat: Redis를 이용해 조회수 확인 API 설

---
+++ service/view/src/main/java/board/view/repository/ArticleViewCountBackUpRepository.java
@@ -0,0 +1,20 @@
+package board.view.repository;
+
+import board.view.entity.ArticleViewCount;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleViewCountBackUpRepository extends JpaRepository<ArticleViewCount, Long> {
+    @Query(
+            value = "update article_view_count set view_count = :viewCount " +
+                    "where article_id = :articleId and view_count < :viewCount",
+            nativeQuery = true
+    )
+    @Modifying
+    int updateViewCount(
+            @Param("articleId") Long articleId,
+            @Param("viewCount") Long viewCount
+    );
+}
