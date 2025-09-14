0976a31f576c509f4679f5c1a072db8568d158d6
kseysh <kseysh123@naver.com>
2025-09-15 00:34:33 +0900

    feat: 조회수 증가시 동시성 문제를 분산락으로 해결

--- service/view/src/main/java/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/board/view/service/ArticleViewService.java
@@ -1,6 +1,8 @@
 package board.view.service;
 
 import board.view.repository.ArticleViewCountRepository;
+import board.view.repository.ArticleViewDistributedLockRepository;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +11,16 @@ import org.springframework.stereotype.Service;
 public class ArticleViewService {
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
     private static final int BACK_UP_BATCH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if(!articleViewDistributedLockRepository.lock(articleId, userId, TTL)){
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BATCH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);
